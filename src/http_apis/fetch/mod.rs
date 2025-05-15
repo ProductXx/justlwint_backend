e43c1ae63203932bfd7a4c5fab2944fd4d48dc08
Linus Walker <rissk_it@proton.me>
2025-05-15 20:20:50 +0630

    chore: fetch profiles

--- src/http_apis/fetch/mod.rs
+++ src/http_apis/fetch/mod.rs
@@ -1,2 +1,3 @@
 pub mod get_nf_posts;
+pub mod profile;
 pub mod scope;
---
+++ src/http_apis/fetch/profile.rs
@@ -0,0 +1,18 @@
+use actix_web::{HttpResponse, get, web::Path};
+
+use crate::structures::{post_structures::NewFeedStruct, static_vars::DB};
+
+#[get("/user/{uid}")]
+pub async fn get_newfeed_posts(uid: Path<String>) -> HttpResponse {
+    let surql = "SELECT * FROM type::thing($uid) LIMIT 50;";
+
+    let mut resul = DB
+        .query(surql)
+        .bind(("uid", uid.into_inner()))
+        .await
+        .unwrap();
+
+    let feed_posts = resul.take::<Vec<NewFeedStruct>>(0).unwrap();
+
+    HttpResponse::Ok().json(feed_posts)
+}
