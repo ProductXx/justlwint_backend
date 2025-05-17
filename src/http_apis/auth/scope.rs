054cafbcde2e8253cbe5b08fb9432ad0d27beac3
Linus Walker <rissk_it@proton.me>
2025-05-17 19:05:12 +0630

    feat: verify otp

--- src/http_apis/auth/scope.rs
+++ src/http_apis/auth/scope.rs
@@ -1,13 +1,11 @@
 use actix_web::{Scope, web};
 
-use super::{
-    login::{login_fn, token_test},
-    sign_up::create_acc,
-};
+use super::{login::login_fn, sign_up::create_acc, verify_otp::verify_otp};
 
 pub fn auth_scope() -> Scope {
     web::scope("/auth")
         .service(login_fn)
         .service(create_acc)
-        .service(token_test)
+        // .service(token_test)
+        .service(verify_otp)
 }
